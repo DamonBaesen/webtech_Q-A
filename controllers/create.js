83337c94bc449f921e57d7622498dbfb42353339
kms26 <kms26@MacBook-Pro-Damon-Baesen.local>
2016-05-10 12:44:06 +0200

    show discussion on personal page

---
+++ controllers/create.js
@@ -0,0 +1,22 @@
+var Discussion = require('../models/discussion');
+
+function create(req, res) {
+    var discussionText = req.body.questionName;
+
+    var newQuestion = new Discussion({
+        moderator: "Damon",
+        question: discussionText,
+        date: Date.now(),
+        place: "Mechelen"
+    });
+
+    newQuestion.save(function (err, message) {
+        if (err) {
+            return console.error(err);
+        }
+        res.redirect("/");
+    });
+
+};
+
+module.exports.create = create;
--- routers/discussion.js
+++ routers/discussion.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var controller = require('../controllers/index');
+var discussions = "";
 
 
 // the root url, if requested we will render the index page
-router.get('/', function(req, res) {
+router.get('/:id', function(req, res) {
+  var discussionID = req.params.id;
+console.log(req.params.id);
+  controller.getAllDiscussions(function (response) {
+    discussions = response;
+  });
+
   res.render('discussion', {
-    title: 'Live Q&A'
+    title: 'Live Q&A',
+    discussions: discussions,
+    discussionID: discussionID
   });
 });
 
